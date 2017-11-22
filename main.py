98ff8984fee31c47af708846a244ba3ce26207a5
Evan Rosenman <etrrosenman@gmail.com>
2017-11-22 02:31:04 -0800

    some updated code

--- main.py
+++ main.py
@@ -57,7 +57,9 @@ def constructDesignMatrix(predictors, precinctData):
 	return np.matrix(vectors).T	
 
 # iterate through the files 
+logLikelihood = 0.0
 for countyFile in countyFiles:
+	print(logLikelihood)
 
 	# read in all the county data as 
 	data = pd.read_csv('../Statewide/'+countyFile, sep = '\t', header = None)
@@ -73,16 +75,47 @@ for countyFile in countyFiles:
 		else:
 			q = int(r.group(1))
 			return currentName.replace(r.group(0), getElectionName(q))
-
 	electionMap = pd.read_csv('../Statewide/' + countyFile.replace('FVE', 'Election Map'), 
 		sep = '\t', header = None)
 	electionMap.columns = ['County', 'Election Number', 'Election Name', 'Election Date']
 	data.columns = [makeReplaceName(s) for s in data.columns.values]
-	
-	# iterate through the precincts
-	precincts = np.unique(data['Precinct Code'])
+
+	# pull the relevant county results 
 	county = countyFile.replace(' FVE 20171016.txt', '')
+	print county
 	countyCode = countyMapping[countyMapping['County'] == county.title()]['ID'].values[0]
+	countyElectionResults = electionResults[(electionResults['County Code'] == countyCode) & 
+		(electionResults['Candidate Office Code'] == 'USP')]
+	trumpCountyVotes = countyElectionResults[countyElectionResults['Candidate Last Name'] == 'TRUMP']
+	clintonCountyVotes = countyElectionResults[countyElectionResults['Candidate Last Name'] == 'CLINTON']
+
+	# pull the precinct mapping
+	zoneCodes = pd.read_csv('../Statewide/' + countyFile.replace('FVE', 'Zone Codes'), 
+		sep = '\t', header = None)
+	zoneCodes.columns = ['County', 'Column', 'Value', 'Precinct Name']
+	zoneCodes = zoneCodes[zoneCodes['Column'] == 1]
+
+	# THIS IS AN EXTREMELY HACKY WAY TO TRY TO PAPER OVER SOME OF THE #
+	# MAPPING ISSUES AND I HAVE NO IDEA IF IT WILL WORK BUT OH WELL   #
+	precincts = np.unique(data['District 1'])
+	if len(precincts) != len(zoneCodes):
+		print 'SOMETHING IS WRONG'
+		t()
+	zoneCodes = zoneCodes.sort('Precinct Name')
+	trumpCountyVotes = trumpCountyVotes.sort('Municipality Name')
+	clintonCountyVotes = clintonCountyVotes.sort('Municipality Name')
+	trumpCountyVotes['Precinct'] = zoneCodes['Precinct Name'].values
+	clintonCountyVotes['Precinct'] = zoneCodes['Precinct Name'].values
+	
+	for i in range(len(trumpCountyVotes['Precinct'])):	# weak sanity checks
+		t1 = trumpCountyVotes['Municipality Name'].values[i]
+		t2 = re.sub('[^a-zA-Z ]', '', trumpCountyVotes['Precinct'].values[i]).strip()
+		c1 = clintonCountyVotes['Municipality Name'].values[i]
+		c2 = re.sub('[^a-zA-Z ]', '', clintonCountyVotes['Precinct'].values[i]).strip()
+#		if (t1 != t2) or (c1 != c2):
+#			print t1, t2, c1, c2
+
+	# iterate through the precincts
 	for precinct in precincts:
 
 		# get the people who voted in 2016 in this precinct
@@ -95,4 +128,16 @@ for countyFile in countyFiles:
 		probabilities = 1/(1 + np.exp(-designMatrix.dot(parameterValues)))
 
 		# pull the actual trump-clinton vote share
-		t()
+		precinctName = zoneCodes[pd.to_numeric(zoneCodes['Value']) == precinct]['Precinct Name'].values[0]
+		trumpVotes = trumpCountyVotes[trumpCountyVotes['Precinct'] == precinctName]['Vote Total'].values[0]
+		clintonVotes = clintonCountyVotes[clintonCountyVotes['Precinct'] == precinctName]['Vote Total'].values[0]
+
+		# if the total trump-clinton votes are off from the vote total by more than 10%, yell
+#		if abs(probabilities.shape[1]/(trumpVotes + clintonVotes) - 1) > 0.1:
+#			print('issue!')
+
+		# get the result
+		pb = PoiBin(probabilities.tolist()[0])
+		logLikelihood += np.log(max(2e-16, pb.pmf(int(round(trumpVotes/float(trumpVotes + clintonVotes) * probabilities.shape[1])))))
+
+
