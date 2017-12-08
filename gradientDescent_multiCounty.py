e4aa6e92f802f00a0817cc1ed74d98f87b9a5baa
Genoc <nitin.viswanathan@gmail.com>
2017-12-08 03:25:26 +0000

    updates

--- gradientDescent_multiCounty.py
+++ gradientDescent_multiCounty.py
@@ -13,7 +13,7 @@ from pdb import set_trace as t
 
 # constants
 countyList = ['BEDFORD', 'ADAMS', 'CHESTER']#, 'ALLEGHENY']
-numIterations = 10000
+numIterations = 1000
 lr = 1e-3
 interceptByCounty = True
 
@@ -37,6 +37,8 @@ def checkMatch(list, string):
 	return np.sum([1 if l in string else 0 for l in list])
 countyFiles = [x for x in arr if 'FVE 20171016.txt' in x and checkMatch(countyList, x) == 1]
 
+countyFiles = ['ADAMS FVE 20171016.txt', 'ALLEGHENY FVE 20171016.txt', 'ARMSTRONG FVE 20171016.txt', 'BEAVER FVE 20171016.txt', 'BEDFORD FVE 20171016.txt', 'BLAIR FVE 20171016.txt', 'BRADFORD FVE 20171016.txt', 'BUTLER FVE 20171016.txt', 'CAMBRIA FVE 20171016.txt', 'CAMERON FVE 20171016.txt', 'CARBON FVE 20171016.txt', 'CHESTER FVE 20171016.txt', 'CLEARFIELD FVE 20171016.txt', 'CLINTON FVE 20171016.txt', 'COLUMBIA FVE 20171016.txt', 'CRAWFORD FVE 20171016.txt', 'ERIE FVE 20171016.txt', 'FOREST FVE 20171016.txt', 'FRANKLIN FVE 20171016.txt', 'GREENE FVE 20171016.txt', 'INDIANA FVE 20171016.txt', 'JEFFERSON FVE 20171016.txt', 'JUNIATA FVE 20171016.txt', 'LAWRENCE FVE 20171016.txt', 'LEBANON FVE 20171016.txt', 'LUZERNE FVE 20171016.txt', 'LYCOMING FVE 20171016.txt', 'MERCER FVE 20171016.txt', 'MIFFLIN FVE 20171016.txt', 'MONROE FVE 20171016.txt', 'MONTOUR FVE 20171016.txt', 'McKEAN FVE 20171016.txt', 'PERRY FVE 20171016.txt', 'PHILADELPHIA FVE 20171016.txt', 'PIKE FVE 20171016.txt', 'POTTER FVE 20171016.txt', 'SCHUYLKILL FVE 20171016.txt', 'SNYDER FVE 20171016.txt', 'SOMERSET FVE 20171016.txt', 'SULLIVAN FVE 20171016.txt', 'SUSQUEHANNA FVE 20171016.txt', 'UNION FVE 20171016.txt', 'WARREN FVE 20171016.txt', 'WASHINGTON FVE 20171016.txt', 'WYOMING FVE 20171016.txt', 'YORK FVE 20171016.txt']
+
 # pre-process for future usage 
 allData = util.preProcess(countyFiles, vfColumnNames, countyMapping, \
 	electionResults, predictors, countyList, interceptByCounty)
@@ -44,13 +46,13 @@ allData = util.preProcess(countyFiles, vfColumnNames, countyMapping, \
 # training loop
 #print util.computeReferenceLikelihood(allData, parameterValues)
 for i in range(numIterations):
-
+	# print('Iteration ' + str(i))
 	# choose a random precinct from a random county
 	county = random.choice(allData.keys())
 	precinct = random.choice(allData[county].keys())
 
 	# make periodic updates
-	if i % 20 == 0:
+	if i % 1000 == 0:
 		l = util.computeLikelihood(allData, parameterValues)
 		with open('trainingPath.txt', 'a') as the_file:
 			the_file.write('%s \n' % l)
